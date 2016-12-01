1109df6cf7595e4b406b48cf818efe3d768e12ee
Your Name <marvin@giveme5ive.com>
2016-12-01 14:31:07 -0500

    push local files

---
+++ app.js
@@ -0,0 +1,13 @@
+var townSquareApp = angular.module('townSquareApp', ['ngRoute', 'ngResource']);
+
+townSquareApp.config(function ($routeProvider) {
+   
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'partials/home.html',
+        controller: 'settingController'
+    })
+    
+	
+});
---
+++ controllers.js
@@ -0,0 +1,32 @@
+//CONTROLLER	
+townSquareApp.controller('settingController', ['$scope', '$http',  function($scope, $http){
+
+ $http.get('/assets/json/socialmedia.json')
+	       .then(function(res){
+	          $scope.socialmedia = res.data;                
+	       });
+
+$scope.iconColor;
+$scope.screenname;
+
+$scope.changeSocialColor = function (value) {
+        //alert(value);
+        if(value === 2){
+        	$scope.iconColor = "default";
+        } else {
+        	$scope.iconColor = "fullColor";
+        }
+    }
+
+
+$scope.getScreenname = function(screenname){
+	//console.log(screenname);
+}    
+
+/*
+$scope.save = function(){
+
+}    
+*/
+
+}]);
