4065639c4526b597f6c3f38a12a50c1b9ed0c9f1
Your Name <marvin@giveme5ive.com>
2016-12-02 10:41:31 -0500

    preview social media icons

--- controllers.js
+++ controllers.js
@@ -9,6 +9,10 @@ townSquareApp.controller('settingController', ['$scope', '$http',  function($sco
 $scope.iconColor;
 $scope.screenname;
 
+
+
+
+
 $scope.changeSocialColor = function (value) {
         //alert(value);
         if(value === 2){
@@ -18,15 +22,41 @@ $scope.changeSocialColor = function (value) {
         }
     }
 
-
+/*
 $scope.getScreenname = function(screenname){
 	//console.log(screenname);
-}    
+} 
+*/   
 
-/*
+/* Need more time to think this logic through
 $scope.save = function(){
 
-}    
-*/
+$scope.socialMediaObj = {}
+$scope.socialMediaArr = []	
+
+  var elements = document.getElementById("social-form-info").elements;
+
+	for (var i = 0, element; element = elements[i++];) {
+	    if (element.value !== "" && element.id !== ""){
+
+	    	$scope.socialMediaObj = {
+					name:"",
+					url:null
+				}
+
+	    		$scope.socialMediaObj.name = element.id,
+	    		$scope.socialMediaObj.url = element.value
+
+             //console.log('element.value ' + element.value)
+	         //console.log('element.id ' +element.id)
+	         $scope.socialMediaArr.push($scope.socialMediaObj);
+	    }
+	}
+
+	//console.log($scope.socialMediaArr);
+	$scope.socialMediaUrls = $scope.socialMediaArr;
+}   
+*/ 
+
 
 }]);
