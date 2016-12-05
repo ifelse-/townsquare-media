91d6a83402088d3d66fad441c9988b00e6e32b46
Your Name <marvin@giveme5ive.com>
2016-12-05 09:26:58 -0500

    change copy

--- directives.js
+++ directives.js
@@ -1,6 +1,6 @@
 townSquareApp.directive("stateDropDown", function() {
     return {
         restrict : "E",
-        templateUrl : "/directives/states.html"
+        templateUrl : "directives/states.html"
     };
 });
